e6e80b5fa50c9c17762188e27af1dcaf7316766c
Hazel Koehler <acomputerdog@gmail.com>
2022-12-02 13:15:24 -0500

    Create attributes for JSON-LD

---
+++ ActivityPub.Types/Attributes/ContextAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace ActivityPub.Types.Attributes;
+
+/// <summary>
+/// Specifies the context to use for a type.
+/// Can be overridden per-property by <see cref="JsonLDPropertyAttribute"/>
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class ContextAttribute : Attribute
+{
+    public string ContextUrl { get; }
+    public ContextAttribute(string contextUrl) => ContextUrl = contextUrl;
+}
---
+++ ActivityPub.Types/Attributes/JsonLDPropertyAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace ActivityPub.Types.Attributes;
+
+/// <summary>
+/// Identifies a property or field as being a JSON-LD property.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class JsonLDPropertyAttribute : Attribute
+{
+    /// <summary>
+    /// If set, specifies the JSON property that maps to this field.
+    /// </summary>
+    public string? Name { get; }
+    
+    /// <summary>
+    /// If set, specifies the context that defines this property.
+    /// Will be used to generate the @context property during serialization, and for mapping during deserialization.
+    /// Overrides the default set by <see cref="ContextAttribute"/>
+    /// </summary>
+    public string? Context { get; }
+    
+    public JsonLDPropertyAttribute(string? name = null, string? context = null)
+    {
+        Name = name;
+        Context = context;
+    }
+}
