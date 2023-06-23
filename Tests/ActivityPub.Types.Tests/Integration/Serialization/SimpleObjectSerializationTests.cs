eb61f088f0ec277b9a9eeab83708970c3bdebcdc
Hazel Koehler <acomputerdog@gmail.com>
2023-06-23 19:06:26 -0400

    Fix serialization tests

--- Tests/ActivityPub.Types.Tests/Integration/Serialization/SimpleObjectSerializationTests.cs
+++ Tests/ActivityPub.Types.Tests/Integration/Serialization/SimpleObjectSerializationTests.cs
@@ -5,25 +5,29 @@ public class SimpleObjectSerializationTests
     public class EmptyObject
     {
         protected readonly ASObject ObjectUnderTest = new();
-        protected Dictionary<string, object?> JsonUnderTest => SerializeObject(ObjectUnderTest);
+        protected Dictionary<string, JsonElement> JsonUnderTest => SerializeObject(ObjectUnderTest);
         
         [Fact]
         public void ShouldIncludeContext()
         {
-            JsonUnderTest.Should().Contain("@context", "https://www.w3.org/ns/activitystreams");
+            JsonUnderTest.Should().ContainKey("@context");
+            JsonUnderTest["@context"].ValueKind.Should().Be(JsonValueKind.String);
+            JsonUnderTest["@context"].GetString().Should().Be("https://www.w3.org/ns/activitystreams");
         }
 
         [Fact]
         public void ShouldIncludeType()
         {
-            JsonUnderTest.Should().Contain("type", "Object");
+            JsonUnderTest.Should().ContainKey("type");
+            JsonUnderTest["type"].ValueKind.Should().Be(JsonValueKind.String);
+            JsonUnderTest["type"].GetString().Should().Be("Object");
         }
 
         [Fact]
         public void ShouldNotIncludeOtherFields()
         {
             JsonUnderTest
-                .Where(pair => pair.Value != null) // Default serializer leaves empty fields as "null", so we have to remove them
+                .Where(pair => pair.Value.ValueKind != JsonValueKind.Null) // Default serializer leaves empty fields as "null", so we have to remove them
                 .Should()
                 .HaveCount(2); // This needs to be updated if we ever add more "mandatory" fields
         }
@@ -31,9 +35,9 @@ public class SimpleObjectSerializationTests
     
     // TODO rest of tests here
 
-    protected static Dictionary<string, object?> SerializeObject(object obj)
+    protected static Dictionary<string, JsonElement> SerializeObject(object obj)
     {
         var json = JsonSerializer.Serialize(obj);
-        return JsonSerializer.Deserialize<Dictionary<string, object?>>(json) ?? throw new ApplicationException("Deserialization failed!");
+        return JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json) ?? throw new ApplicationException("Deserialization failed!");
     }
 }
