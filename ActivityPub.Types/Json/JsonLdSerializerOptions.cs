915d1295cf6d2c67cbc88bb75a8b3a69c953c287
Hazel Koehler <acomputerdog@gmail.com>
2023-06-23 20:19:34 -0400

    Implement JsonLdSerializerOptions to provide recommended default options

---
+++ ActivityPub.Types/Json/JsonLdSerializerOptions.cs
@@ -0,0 +1,23 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ActivityPub.Types.Json;
+
+/// <summary>
+/// Recommended JSON serializer options for working with JSON-LD
+/// </summary>
+public static class JsonLdSerializerOptions
+{
+    public static JsonSerializerOptions Default => new JsonSerializerOptions().AddJsonLd();
+
+    public static JsonSerializerOptions AddJsonLd(this JsonSerializerOptions options)
+    {
+        options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.PropertyNameCaseInsensitive = true;
+        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        return options;
+    }
+}
