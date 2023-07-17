649db93886fb83f14c9e88b93b6c6bab1acee5cd
Indeedornot <gravitybartek@gmail.com>
2023-07-17 15:27:04 +0200

    fix duplicate license header

--- ActivityPub.Types/Util/JsonLDContext.cs
+++ ActivityPub.Types/Util/JsonLDContext.cs
@@ -3,8 +3,6 @@
 
 using System.Text.Json.Serialization;
 using ActivityPub.Types.Json;
-// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 namespace ActivityPub.Types.Util;
 
