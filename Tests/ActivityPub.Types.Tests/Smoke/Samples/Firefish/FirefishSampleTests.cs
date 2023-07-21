10c2575091b1176ddd5ad70b7c40e1977537fd7a
Hazel Koehler <acomputerdog@gmail.com>
2023-07-20 22:10:39 -0400

    Coalesce smoke test samples to remove duplicated logic
    * Sample JSON is now an embedded resource
    * Samples are grouped by software
    * Calckey renamed to Firefish
    * Serialization is also tested
    
    Part of #58.

---
+++ Tests/ActivityPub.Types.Tests/Smoke/Samples/Firefish/FirefishSampleTests.cs
@@ -0,0 +1,22 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using ActivityPub.Types.Collection;
+using ActivityPub.Types.Extended.Actor;
+using ActivityPub.Types.Tests.Util.Fixtures;
+
+namespace ActivityPub.Types.Tests.Smoke.Samples.Firefish;
+
+public class FirefishSampleTests : SampleTests
+{
+    public FirefishSampleTests(JsonLdSerializerFixture fixture) : base(fixture) {}
+
+    [Fact]
+    public void PersonActorShouldConvert() => TestSample(typeof(PersonActor), PersonActor.PersonType);
+
+    [Fact]
+    public void ASOrderedCollectionShouldConvert() => TestSample(typeof(ASOrderedCollection<ASType>), CollectionTypes.OrderedCollectionType);
+
+    [Fact]
+    public void ASOrderedCollectionPageShouldConvert() => TestSample(typeof(ASOrderedCollectionPage<ASType>), CollectionTypes.OrderedCollectionPageType);
+}
