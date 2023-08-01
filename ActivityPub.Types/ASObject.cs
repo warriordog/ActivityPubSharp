ac6478f77bdb04313072ec930ca075c9866cc8cb
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-08-01 19:59:48 +0700

    Update ActivityPub.Types/ASObject.cs

--- ActivityPub.Types/ASObject.cs
+++ ActivityPub.Types/ASObject.cs
@@ -15,7 +15,7 @@ namespace ActivityPub.Types;
 /// The Object type serves as the base type for most of the other kinds of objects defined in the Activity Vocabulary, including other Core types such as Activity, IntransitiveActivity, Collection and OrderedCollection. 
 /// </summary>
 /// <seealso href="https://www.w3.org/TR/activitystreams-vocabulary/#dfn-object" />
-[ASTypeKey(ObjectType)]
+[APType(ObjectType)]
 public class ASObject : ASType
 {
     public const string ObjectType = "Object";
