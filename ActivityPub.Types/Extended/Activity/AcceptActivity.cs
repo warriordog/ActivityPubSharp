2c559a2b2113313ee675c028257c837bc29a5988
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-08-01 20:04:11 +0700

    Update ActivityPub.Types/Extended/Activity/AcceptActivity.cs

--- ActivityPub.Types/Extended/Activity/AcceptActivity.cs
+++ ActivityPub.Types/Extended/Activity/AcceptActivity.cs
@@ -10,7 +10,7 @@ namespace ActivityPub.Types.Extended.Activity;
 /// Indicates that the actor accepts the object.
 /// The target property can be used in certain circumstances to indicate the context into which the object has been accepted. 
 /// </summary>
-[ASTypeKey(AcceptType)]
+[APType(AcceptType)]
 public class AcceptActivity : ASTransitiveActivity
 {
     public const string AcceptType = "Accept";
