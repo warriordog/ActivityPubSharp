65af344fba7a9cccfeb5266110816af8b51ced5e
Hazel Koehler <acomputerdog@gmail.com>
2023-08-01 22:37:38 -0400

    Implement new version of ASTypeConverter

---
+++ ActivityPub.Types/Conversion/ASTypeConverter.cs
@@ -0,0 +1,41 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using InternalUtils;
+
+namespace ActivityPub.Types.Conversion;
+
+public class ASTypeConverter : JsonConverterFactory
+{
+    private readonly Type _asTypeType = typeof(ASType);
+
+
+    public override bool CanConvert(Type typeToConvert) => typeToConvert.IsAssignableTo(_asTypeType);
+
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        // Create an instance of the generic converter
+        var constructedType = typeof(ASTypeConverter<>).MakeGenericType(typeToConvert);
+        return (JsonConverter)Activator.CreateInstance(constructedType)!;
+    }
+}
+
+internal class ASTypeConverter<T> : JsonConverter<T>
+    where T : ASType
+{
+    private readonly Func<TypeMap, T> _typeConstructor = TypeUtils.CreateDynamicConstructor<TypeMap, T>();
+
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var typeMap = JsonSerializer.Deserialize<TypeMap>(ref reader, options)
+                      ?? throw new JsonException($"Can't convert {typeof(T)}: conversion to TypeMap returned null");
+
+        return _typeConstructor(typeMap);
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        // Simple pass-through of TypeMap
+        => JsonSerializer.Serialize(writer, value.TypeMap, options);
+}
