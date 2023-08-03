72a271aadcca62269efeb3941bdab8167cac965e
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-08-03 20:41:05 +0700

    Update ActivityPub.Types/Extended/Activity/InviteActivity.cs

--- ActivityPub.Types/Extended/Activity/InviteActivity.cs
+++ ActivityPub.Types/Extended/Activity/InviteActivity.cs
@@ -9,7 +9,7 @@ namespace ActivityPub.Types.Extended.Activity;
 /// <summary>
 /// A specialization of Offer in which the actor is extending an invitation for the object to the target. 
 /// </summary>
-[ASTypeKey(InviteType)]
+[APType(InviteType)]
 public class InviteActivity : OfferActivity
 {
     public const string InviteType = "Invite";
