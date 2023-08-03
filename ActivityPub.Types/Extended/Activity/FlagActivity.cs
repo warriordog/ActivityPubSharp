9393a212a188c7d1518f0e4d1c11dd6bb6ac3a86
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-08-03 20:39:25 +0700

    Update ActivityPub.Types/Extended/Activity/FlagActivity.cs

--- ActivityPub.Types/Extended/Activity/FlagActivity.cs
+++ ActivityPub.Types/Extended/Activity/FlagActivity.cs
@@ -10,7 +10,7 @@ namespace ActivityPub.Types.Extended.Activity;
 /// Indicates that the actor is "flagging" the object.
 /// Flagging is defined in the sense common to many social platforms as reporting content as being inappropriate for any number of reasons. 
 /// </summary>
-[ASTypeKey(FlagType)]
+[APType(FlagType)]
 public class FlagActivity : ASTransitiveActivity
 {
     public const string FlagType = "Flag";
