babd4ccf75851fe18aae67ddc973a3afeda723d9
Hazel Koehler <acomputerdog@gmail.com>
2023-11-29 09:06:16 -0500

    expose toggle for snapshot publish

---
+++ Tool/Publish-Package.ps1
@@ -0,0 +1,64 @@
+# Usage: update version in all csproj files, then run this from the repository root.
+
+[CmdLetBinding(PositionalBinding = $false)]
+param (
+    [Parameter(
+        HelpMessage = "URL of a NuGet source repository. Defaults to nuget.org."
+    )]
+    [string] $NugetSource = "https://api.nuget.org/v3/index.json",
+    
+    [Parameter(
+        Mandatory,
+        HelpMessage = "API key for the NuGet repository."
+    )]
+    [string] $NugetKey,
+    
+    [Parameter(
+        HelpMessage = "Name of the build configuration to run. Defaults to Release."
+    )]
+    [string] $BuildConfig = "Release",
+    
+    [Parameter(
+        Mandatory,
+        Position = 0,
+        ValueFromRemainingArguments,
+        HelpMessage = "Names of packages (projects) to upload, separated by commas or spaces."
+    )]
+    [string[]] $PackageNames,
+
+    [Parameter(
+        HelpMessage = "Uploads a 'snapshot' build, which includes a timestamp suffix"
+    )]
+    [switch] $Snapshot
+);
+
+$versionSuffix = "";
+if ($Snapshot)
+{
+    $timestamp = Get-Date -Format "yyyy-MM-dd.HH-mm-ss.fff";
+    $versionSuffix = "snapshot.$timestamp";
+}
+
+# Workaround for https://github.com/dotnet/docs/issues/12304
+Write-Output "## [publish-package] Purge old NuGet builds";
+foreach ($packageName in $PackageNames)
+{
+    $oldNugets = Get-ChildItem -Path "Source\$packageName\bin\$buildConfig" -Include "*.nupkg" -Recurse;
+    Remove-Item $oldNugets;
+}
+
+Write-Output "";
+Write-Output "## [publish-package] Build and pack selected packages";
+& dotnet restore /p:VersionSuffix=$versionSuffix
+& dotnet clean --configuration $BuildConfig --version-suffix $versionSuffix;
+& dotnet build --configuration $BuildConfig --version-suffix $versionSuffix;
+& dotnet pack --configuration $BuildConfig --version-suffix $versionSuffix;
+
+foreach ($packageName in $PackageNames)
+{
+    $packagePath = "Source\$packageName\bin\$buildConfig\*.nupkg";
+
+    Write-Output "";
+    Write-Output "## [publish-package] Push $packageName";
+    & dotnet nuget push $packagePath --api-key $NugetKey --source $NugetSource;
+}
---
+++ Tool/Publish-Solution.ps1
@@ -0,0 +1,42 @@
+﻿[CmdLetBinding(PositionalBinding = $false)]
+param (
+    [Parameter(
+        HelpMessage = "URL of a NuGet source repository. Defaults to nuget.org."
+    )]
+    [string] $NugetSource = "https://api.nuget.org/v3/index.json",
+    
+    [Parameter(
+        Mandatory,
+        HelpMessage = "API key for the NuGet repository."
+    )]
+    [string] $NugetKey,
+    
+    [Parameter(
+        HelpMessage = "Name of the build configuration to run. Defaults to Release."
+    )]
+    [string] $BuildConfig = "Release",
+
+    [Parameter(
+        HelpMessage = "Uploads a 'snapshot' build, which includes a timestamp suffix"
+    )]
+    [switch] $Snapshot
+);
+
+$packageNames =
+@(
+    "ActivityPub.Client",
+    "ActivityPub.Common",
+    "ActivityPub.Extension.Mastodon",
+    "ActivityPub.Server",
+    "ActivityPub.Server.AspNetCore",
+    "ActivityPub.Types"
+);
+
+Write-Output "## [Publish-Solution] Publish entire solution";
+Write-Output "";
+& $PSScriptRoot\Publish-Package `
+    -NugetSource $NugetSource `
+    -NugetKey $NugetKey `
+    -BuildConfig $BuildConfig `
+    -Snapshot:$Snapshot `
+    $packageNames;
